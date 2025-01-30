e3a7529af01bbd6d43ca771326db6a8e90510613
luna :3 <ellipticobj@gmail.com>
2025-01-30 21:44:40 +0800

    input output done!!

--- evaluator.py
+++ evaluator.py
@@ -10,11 +10,14 @@ class Evaluator:
             self.evalnode(node)
 
     def evalnode(self, node):
+        '''
+        parses the nodes n stuff
+        '''
         if node[0] == "DECLARE":
-            pass
+            self.decl(node)
 
         elif node[0] == "IF":
-            pass
+            self.ifelse(node)
         
         elif node[0] == "WHILE":
             pass
@@ -30,6 +33,13 @@ class Evaluator:
 
         else:
             raise RuntimeError(f"unknown node type: {node[0]}")
+        
+    def evalcond(self, condition):
+        '''
+        evaluates conditions
+        '''
+        # TODO: make this actually work???
+        return eval(condition)
     
     def decl(self, node):
         _, varname, vartype, value, isconst = node
@@ -48,5 +58,43 @@ class Evaluator:
     def ifelse(self, node):
         _, condition, body, elifs, elsebod = node
 
-        pass
+        if self.evalcond(condition):
+            self.evaluate(body)
+            return
+        
+        for elifcond, elifbod in elifs:
+            if self.evalcond(elifcond):
+                self.evaluate(body)
+                return
+            
+        self.evaluate(elsebod)
     
+    def whileloop(self, node):
+        _, condition, body = node
+
+        while self.evalcond(condition):
+            self.evaluate(body)
+            
+    def output(self, node):
+        _, value = node
+
+        print(value)
+        
+    def receive(self, node):
+        _, varname = node
+        usrinp = input()
+        try:
+            variables[varname][0] = [usrinp]
+        except:
+            print(f"variable {varname} does not exist")
+            
+    def assign(self, node):
+        _, name, value = node
+
+        if name in constants:
+            raise RuntimeError(f"constant {name} cannot be changed")
+        
+        try:
+            variables[name][0] = value
+        except:
+            print(f"variable {name} does not exist. ")
