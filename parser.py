4ce5c24f3dcd75e4b985f5362cba411f65e41d24
luna :3 <ellipticobj@gmail.com>
2025-01-30 15:24:59 +0800

    declaration logic

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+from const import *
+
 class ParseError(Exception):
     def __init__(self, message = ""):
         super().__init__(message)
@@ -15,9 +17,9 @@ class Parser:
     
     def consume(self, expected=None:
         token = self.curr()
-        if token is None:
+        if not token:
             raise ParseError("unexpected eol")
-            
+        
         if expected and token[0] != expected:
             raise ParseError(f"expected {expected} but got {token[0]} instead")
             
@@ -33,31 +35,66 @@ class Parser:
     def parseline(self):
         # TODO
         token = self.curr()
-        if token[0] == 'NEW':
-            pass
+        if token[0] == NEW_VAR_IDENT.upper():
+            return self.declaration()
         
-        elif token[0] == 'IF':
-            pass
+        elif token[0] == IF_OPEN.upper():
+            return self.ifelse()
         
-        elif token[0] == 'WHILE':
-            pass
+        elif token[0] == WHILE_OPEN.upper():
+            return self.whileloop()
         
-        elif token[0] == 'PRINT':
-            pass
+        elif token[0] == OUTPUT.upper():
+            return self.output()
         
-        elif token[0] == 'RECEIVE':
-            pass
+        elif token[0] == INPUT.upper():
+            return self.input()
         
         elif token[0] == 'ID':
-            pass
+            return self.assignment()
         
         else:
             raise(ParseError(f"unexpected token: {token[0]}"))
         
     def declaration(self):
-        pass
+        self.consume(NEW_VAR_IDENT.upper())
+        isconst = False
+
+        if self.curr()[1] in CONST_TYPES:
+            self.consume()
+            isconst = True
+
+        elif self.curr()[1] in VAR_TYPES:
+            self.consume()
+        
+        else:
+            raise ParseError("expected variable or constant type")
+            
+        vartype = self.consume("ID")[1]
+        varname = self.consume("ID")[1]
+        
+        value = None
+
+        if self.curr() and self.curr()[1] == ASSIGNMENT_OPERATOR:
+            self.consume()
+            value = self.expr()
+            
+        return ("DECLARE", varname, vartype, value, isconst)
 
     def ifelse(self):
         pass
 
-    def 
+    def whileloop(self):
+        pass
+        
+    def output(self):
+        pass
+        
+    def input(self):
+        pass
+        
+    def assignment(self):
+        pass
+    
+    def expr(self):
+        pass
