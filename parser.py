8c50410e54d12895ff5232d5c5c7327facd54540
luna :3 <ellipticobj@gmail.com>
2025-01-30 15:11:02 +0800

    started on parser

--- parser.py
+++ parser.py
@@ -8,4 +8,56 @@ class Parser:
         self.idx = 0
         
     def curr(self):
-        return self.tokens[self.pos] if self.pon < len(self.tokens) else None
+        '''
+        returns the current token without consumitng
+        '''
+        return self.tokens[self.pos] if self.pos < len(self.tokens) else None
+    
+    def consume(self, expected=None:
+        token = self.curr()
+        if token is None:
+            raise ParseError("unexpected eol")
+            
+        if expected and token[0] != expected:
+            raise ParseError(f"expected {expected} but got {token[0]} instead")
+            
+        self.pos += 1
+        return token
+    
+    def parse(self):
+        ast = []
+        while self.curr():
+            ast.append(self.parseline())
+        return ast
+
+    def parseline(self):
+        # TODO
+        token = self.curr()
+        if token[0] == 'NEW':
+            pass
+        
+        elif token[0] == 'IF':
+            pass
+        
+        elif token[0] == 'WHILE':
+            pass
+        
+        elif token[0] == 'PRINT':
+            pass
+        
+        elif token[0] == 'RECEIVE':
+            pass
+        
+        elif token[0] == 'ID':
+            pass
+        
+        else:
+            raise(ParseError(f"unexpected token: {token[0]}"))
+        
+    def declaration(self):
+        pass
+
+    def ifelse(self):
+        pass
+
+    def 
