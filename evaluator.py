c302e570527826459bb7cda4536d0c3f4d0d49a6
luna :3 <ellipticobj@gmail.com>
2025-02-01 00:14:32 +0800

    yes!!!!

--- evaluator.py
+++ evaluator.py
@@ -8,7 +8,48 @@ class Evaluator:
     def evaluate(self, ast):
         for node in ast:
             self.evalnode(node)
-  
+            
+    def evalnode(self, node):
+        type_ = node.get("type")
+        
+        if type_ == "declaration":
+            self.decl(node)
+            
+        elif type_ == "if":
+            self.ifelse(node)
+            
+        elif type_ == "while":
+            self.whileloop(node)
+            
+        elif type_ == "output":
+            self.output(node)
+            
+        elif type_ == "input":
+            self.receive(node)
+            
+        elif type_ == "reassignment":
+            self.reassign(node)
+        
+        else:
+            raise RuntimeError(f"unknown node type {type_}\nthis is most likely a problem with sigmalang. open an issue at https://github.com/dimini171/sigma/issues/new")
+        
+    def decl(self, node):
+        print(f"declaration {node}")
+        
+    def ifelse(self, node):
+        print(f"if {node}")
+        
+    def whileloop(self, node):
+        print(f"while {node}")
+        
+    def output(self, node):
+        print(f"output {node}")
+        
+    def receive(self, node):
+        print(f"output: {node}")
+        
+    def reassign(self, node):
+        print(f"reassign {node}")
   
 from lexer import *
 from parser import *
