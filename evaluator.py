50d84839bcc09b830e9ae9f813156f4ed7ed7827
luna :3 <ellipticobj@gmail.com>
2025-02-01 00:06:27 +0800

    rewriting + test cases cleanup

--- evaluator.py
+++ evaluator.py
@@ -8,112 +8,16 @@ class Evaluator:
     def evaluate(self, ast):
         for node in ast:
             self.evalnode(node)
+  
+  
+from lexer import *
+from parser import *
+      
+test = "print 'some text'"
+lexer = Lexer(test)
+tokens = lexer.tokenize()
+parser = Parser(tokens)
+ast = parser.parse()
 
-    def evalnode(self, node):
-        '''
-        parses the nodes n stuff
-        '''
-        if node[0] == "DECLARE":
-            self.decl(node)
-
-        elif node[0] == "IF":
-            self.ifelse(node)
-        
-        elif node[0] == "WHILE":
-            self.whileloop(node)
-
-        elif node[0] == "PRINT":
-            self.output(node)
-
-        elif node[0] == "RECEIVE":
-            self.receive(node)
-
-        elif node[0] == "ASSIGN":
-            self.assign(node)
-
-        else:
-            raise RuntimeError(f"unknown node type: {node[0]}\n this is most likely a problem with sigma. please open an issue at https://github.com/dimini171/sigma/issues")
-        
-    def evalcond(self, condition):
-        '''
-        evaluates conditions
-        '''
-        # TODO: make this actually work???
-        return eval(condition)
-    
-    def decl(self, node):
-        _, varname, vartype, value, isconst = node
-        
-        # TODO: add type checking
-
-        if varname in constants:
-            raise RuntimeError(f"constant {varname} cannot be changed")
-        
-        if value is not None:
-            variables[varname] = [value, vartype]
-            
-            if isconst:
-                constants[varname] = vartype
-    
-    def ifelse(self, node):
-        _, condition, body, elifs, elsebod = node
-
-        if self.evalcond(condition):
-            self.evaluate(body)
-            return
-        
-        for elifcond, elifbod in elifs:
-            if self.evalcond(elifcond):
-                self.evaluate(elifbod)
-                return
-            
-        self.evaluate(elsebod)
-    
-    def whileloop(self, node):
-        _, condition, body = node
-
-        while self.evalexpr(condition):
-            self.evaluate(body)
-            
-    def output(self, node):
-        _, value = node
-
-        print(value)
-        
-    def receive(self, node):
-        _, varname = node
-        usrinp = input()
-        try:
-            variables[varname][0] = [usrinp]
-        except:
-            print(f"variable {varname} does not exist")
-            
-    def assign(self, node):
-        _, name, value = node
-
-        if name in constants:
-            raise RuntimeError(f"constant {name} cannot be changed")
-        
-        try:
-            variables[name][0] = value
-        except:
-            print(f"variable {name} does not exist. ")
-            
-    def evalexpr(self, expr):
-        if expr[0] == "INTEGER":
-            return int(expr[1])
-        
-        elif expr[0] == "FLOAT":
-            return float(expr[1])
-        
-        elif expr[0] == "STRING":
-            return str(expr[1])
-        
-        elif expr[0] == "VARIABLE":
-            if expr[1] not in variables:
-                raise RuntimeError(f"variable {expr[1]} is undefined")
-            
-            return variables[expr[1]]
-
-        else:
-            raise RuntimeError(f"unknown expression type: {expr[0]}\n this is most likely a problem with sigma. please open an issue at https://github.com/dimini171/sigma/issues")
+evaluator = Evaluator()
+evaluator.evaluate(ast)
